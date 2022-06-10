8e128ade103781ceb64e64dd26c6ed326f1e2ea0
Amanda Bandeira <bandeira.e.amanda@gmail.com>
2022-06-10 18:28:31 -0300

    feat: add update one product service

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+  id: string;
+}
+
+class UpdateProductService {
+  public async execute({
+    name,
+    price,
+    quantity,
+    id,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+    if (!product) {
+      throw new AppError(`Product with id ${id} not found.`);
+    }
+
+    const productExists = await productsRepository.findByName(name);
+    if (productExists && name !== product.name) {
+      throw new AppError(`There is already one product with this name ${name}`);
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+    return product;
+  }
+}
+
+export default UpdateProductService;
