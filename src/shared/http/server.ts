e33d2f3dc1f0c11c3bd3ca4ceec1497fa4b45a6d
Amanda Bandeira <bandeira.e.amanda@gmail.com>
2022-06-15 17:49:29 -0300

    feat: add file static route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,13 +5,14 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import { errors } from 'celebrate';
+import uploadConfig from '@config/upload';
 import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
