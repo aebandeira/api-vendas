83e03d1ef1f7dfe26f846b92e17de666a3f0a414
Amanda Bandeira <bandeira.e.amanda@gmail.com>
2022-09-13 18:49:49 -0300

    refactor: create models folder

--- src/modules/products/infra/http/routes/products.routes.ts
+++ src/modules/products/infra/http/routes/products.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { celebrate, Joi, Segments, errors } from 'celebrate';
+import { celebrate, Joi, Segments } from 'celebrate';
 import ProductsController from '../controllers/ProductsController';
 
 const productsRouter = Router();
